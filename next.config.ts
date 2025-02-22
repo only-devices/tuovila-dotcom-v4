eb0ca4438b3574c6314f73ae5d94eaeb60050790
Eric Tuovila <36007317+only-devices@users.noreply.github.com>
2025-02-21 22:03:57 -0500

    Netlify adjustments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lastfm.freetls.fastly.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.hardcover.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hardcover.app',
+      },
+    ],
+  },
+  webpack: (config) => {
+    // Suppress the punycode deprecation warning
+    config.ignoreWarnings = [
+      { module: /node_modules\/punycode/ }
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
